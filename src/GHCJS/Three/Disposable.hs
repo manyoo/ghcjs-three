e445064926f165feeb0965c4b5ab70e3717b7aaf
Eric Wong <ericsyw@gmail.com>
2015-10-08 14:39:55 +0800

    Add the generic Disposable class and API

---
+++ src/GHCJS/Three/Disposable.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE JavaScriptFFI #-}
+module GHCJS.Three.Disposable (Disposable(..))
+    where
+
+import GHCJS.Types
+import GHCJS.Three.Monad
+
+foreign import javascript unsafe "($1).dispose()"
+    oDispose :: JSRef -> Three ()
+
+class IsJSRef o => Disposable o where
+    dispose :: o -> Three ()
+    dispose = oDispose . jsref
