c7e4f903c1827f756d05d4a070aa685e32b112fe
Eric Wong <ericsyw@gmail.com>
2015-10-09 08:48:26 +0800

    Add Geometry APIs

---
+++ src/GHCJS/Three/Geometry.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE JavaScriptFFI #-}
+module GHCJS.Three.Geometry (
+
+    ) where
+
+import GHCJS.Types
+import qualified GHCJS.Marshal as Marshal
+
+import Data.Maybe (fromMaybe)
+
+import GHCJS.Three.Monad
+import GHCJS.Three.Vector hiding (getObject)
+import GHCJS.Three.Disposable
+
+newtype Geometry a = Geometry {
+    getObject :: Object a
+}
+
+instance ThreeJSRef (Geometry a) where
+    toJSRef = toJSRef . getObject
+    fromJSRef = Geometry . fromJSRef
+
+foreign import javascript unsafe "new window.THREE.Geometry()"
+    thr_mkGeometry :: Three JSRef
+
+mkGeometry :: Three (Geometry ())
+mkGeometry = fromJSRef <$> thr_mkGeometry
+
+-- | get vertices
+foreign import javascript safe "($1).vertices"
+    thr_vertices :: JSRef -> JSRef
+
+-- | set vertices
+foreign import javascript unsafe "($2).vertices = $1"
+    thr_setVectices :: JSRef -> JSRef -> Three ()
+
+-- use Marshal.fromJSRef to convert JSRef -> IO (Maybe [JSRef])
+-- and Marshal.toJSRef to convert [JSRef] -> IO JSRef
+class ThreeJSRef g => IsGeometry g where
+    vertices :: g -> Three [Vector ()]
+    vertices g = (map fromJSRef . fromMaybe []) <$> (Marshal.fromJSRef $ thr_vertices $ toJSRef g)
+
+    setVertices :: [Vector a] -> g -> Three ()
+    setVertices vs g = (Marshal.toJSRef $ map toJSRef vs) >>= flip thr_setVectices (toJSRef g)
+
+instance IsGeometry (Geometry a)
+instance Disposable (Geometry a)
