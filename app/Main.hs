18a3cfb59d18ebb662f95390f8cf1025cecfe00d
Eric Wong <ericsyw@gmail.com>
2015-10-09 15:06:59 +0800

    Add demo app

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,38 @@
 module Main where
 
-import Lib
+import Control.Applicative ((<$>), (<*>))
+import GHCJS.Three
+import GHCJS.DOM
+import GHCJS.DOM.Document
+import GHCJS.DOM.Node
 
-main :: IO ()
-main = someFunc
+import Data.Maybe (fromJust)
+
+main = runWebGUI $ \webview -> do
+  scene <- mkScene
+  camera <- mkPerspectiveCamera 45 1 0.1 1000
+  renderer <- mkWebGLRenderer
+  setSize 500 500 renderer
+  glElem <- domElement renderer
+
+  document <- fromJust <$> webViewGetDomDocument webview
+  docBody <- fromJust <$> getBody document
+  appendChild docBody glElem
+
+  geo <- mkBoxGeometry 1 1 1
+  mat <- mkMeshNormalMaterial
+  c <- mkColor 0.2 0.3 0.5
+  setColor c mat
+  cube <- mkMesh geo mat
+  add cube scene
+
+  let cp = position camera
+  setZ 5 cp
+  setPosition cp camera
+
+  let rf = do
+         let cubeR = rotation cube
+         setX (getX cubeR + 0.01) cubeR
+         setY (getY cubeR + 0.01) cubeR
+         render scene camera renderer
+  runThree rf
