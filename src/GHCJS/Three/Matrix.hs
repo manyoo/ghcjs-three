01d1f79eeb126479fbf6cdf88d8c70d074a6b3ec
Eric Wong <ericsyw@gmail.com>
2016-12-19 15:46:12 +0800

    add new elements API to Matrix

--- src/GHCJS/Three/Matrix.hs
+++ src/GHCJS/Three/Matrix.hs
@@ -1,12 +1,16 @@
 {-# LANGUAGE JavaScriptFFI, GeneralizedNewtypeDeriving #-}
 module GHCJS.Three.Matrix (
-    Matrix(..), mkMatrix
+    Matrix(..), mkMatrix, elements
 ) where
 
 import Data.Functor
 import GHCJS.Types
+import qualified GHCJS.Marshal as M
 
 import GHCJS.Three.Monad
+import Control.Monad
+
+import Data.Maybe
 
 -- haskell wrapper for the Matrix4 type
 newtype Matrix = Matrix {
@@ -19,3 +23,9 @@ foreign import javascript unsafe "new window['THREE']['Matrix4']()"
 
 mkMatrix :: Three Matrix
 mkMatrix = fromJSVal <$> thr_mkMatrix
+
+foreign import javascript unsafe "($1).elements"
+    thr_elements :: JSVal -> JSVal
+
+elements :: Matrix -> Three [Double]
+elements m = (mapM (fmap (fromMaybe 0) . M.fromJSVal) . fromMaybe []) =<< M.fromJSVal (thr_elements $ toJSVal m)
