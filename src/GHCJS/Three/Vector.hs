f900d24d5d1bd3cf878aee76ca3b572ace3175e1
Eric Wong <ericsyw@gmail.com>
2016-12-20 19:38:41 +0800

    Add more Vector APIs

--- src/GHCJS/Three/Vector.hs
+++ src/GHCJS/Three/Vector.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE JavaScriptFFI, GeneralizedNewtypeDeriving #-}
 module GHCJS.Three.Vector (
-    TVector3(..), TVector2(..), NormalVector,
+    IsJSVector(..), TVector3(..), TVector2(..), NormalVector,
     mkTVector3, toVector3, mkTVector2, toVector2, vector3To2, (#+), (#-),
     module Data.Vector.V2,
     module Data.Vector.V3
@@ -9,15 +9,96 @@ module GHCJS.Three.Vector (
 import Data.Functor
 import GHCJS.Types
 
+import JavaScript.Array
+
 import GHCJS.Three.Monad
 import GHCJS.Three.HasXYZ
 import GHCJS.Three.Matrix
+import GHCJS.Three.CanCopy
 
 import Data.Vector.V2
 import Data.Vector.V3
 
--- empty class only used for restricting data types below
-class ThreeJSVal v => IsJSVector v
+foreign import javascript unsafe "($2)['add']($1)"
+    thr_add :: JSVal -> JSVal -> Three ()
+
+foreign import javascript unsafe "($2)['addScalar']($1)"
+    thr_addScalar :: Double -> JSVal -> Three ()
+
+foreign import javascript unsafe "($3)['addVectors']($1, $2)"
+    thr_addVectors :: JSVal -> JSVal -> JSVal -> Three ()
+
+foreign import javascript unsafe "($2)['angleTo']($1)"
+    thr_angleTo :: JSVal -> JSVal -> Double
+
+foreign import javascript unsafe "($2)['cross']($1)"
+    thr_cross :: JSVal -> JSVal -> Three ()
+
+foreign import javascript unsafe "($3)['crossVectors']($1, $2)"
+    thr_crossVectors :: JSVal -> JSVal -> JSVal -> Three ()
+
+foreign import javascript unsafe "($2)['distanceTo']($1)"
+    thr_distanceTo :: JSVal -> JSVal -> Double
+
+foreign import javascript unsafe "($2)['distanceToSquared']($1)"
+    thr_distanceToSquared :: JSVal -> JSVal -> Double
+
+foreign import javascript unsafe "($2)['dot']($1)"
+    thr_dot :: JSVal -> JSVal -> Double
+
+foreign import javascript unsafe "($3)['fromArray']($1, $2)"
+    thr_fromArray :: JSVal -> Int -> JSVal -> Three ()
+
+foreign import javascript unsafe "($1)['length']()"
+    thr_length :: JSVal -> Double
+
+foreign import javascript unsafe "($1)['normalize']()"
+    thr_normalize :: JSVal -> Three ()
+
+foreign import javascript unsafe "($2)['multiplyScalar']($1)"
+    thr_multiplyScalar :: Double -> JSVal -> Three ()
+
+
+class ThreeJSVal v => IsJSVector v where
+    addTo :: v -> v -> Three ()
+    addTo v1 v2 = thr_add (toJSVal v1) (toJSVal v2)
+
+    addScalar :: Double -> v -> Three ()
+    addScalar s v = thr_addScalar s (toJSVal v)
+
+    addVectors :: v -> v -> v -> Three ()
+    addVectors v1 v2 v = thr_addVectors (toJSVal v1) (toJSVal v2) (toJSVal v)
+
+    angleTo :: v -> v -> Double
+    angleTo v1 v2 = thr_angleTo (toJSVal v1) (toJSVal v2)
+
+    cross :: v -> v -> Three ()
+    cross v1 v = thr_cross (toJSVal v1) (toJSVal v)
+
+    crossVectors :: v -> v -> v -> Three ()
+    crossVectors v1 v2 v = thr_crossVectors (toJSVal v1) (toJSVal v2) (toJSVal v)
+
+    distanceTo :: v -> v -> Double
+    distanceTo v1 v2 = thr_distanceTo (toJSVal v1) (toJSVal v2)
+
+    distanceToSquared :: v -> v -> Double
+    distanceToSquared v1 v2 = thr_distanceToSquared (toJSVal v1) (toJSVal v2)
+
+    dot :: v -> v -> Double
+    dot v1 v2 = thr_dot (toJSVal v1) (toJSVal v2)
+
+    fromArray :: JSArray -> Int -> v -> Three ()
+    fromArray arr idx v = thr_fromArray (jsval arr) idx (toJSVal v)
+
+    length :: v -> Double
+    length = thr_length . toJSVal
+
+    normalize :: v -> Three ()
+    normalize = thr_normalize . toJSVal
+
+    multiplyScalar :: Double -> v -> Three ()
+    multiplyScalar s v = thr_multiplyScalar s (toJSVal v)
+
 
 -- JS version of 3D Vector
 newtype TVector3 = TVector3 {
@@ -28,6 +109,8 @@ instance IsJSVector TVector3
 instance HasX TVector3
 instance HasY TVector3
 instance HasZ TVector3
+instance CanApplyMatrix4 TVector3
+instance CanCopy TVector3
 
 foreign import javascript unsafe "($2)['setFromMatrixPosition']($1)"
     thr_setFromMatrixPosition :: JSVal -> JSVal -> Three ()
@@ -46,6 +129,7 @@ newtype TVector2 = TVector2 {
 instance IsJSVector TVector2
 instance HasX TVector2
 instance HasY TVector2
+instance CanCopy TVector2
 
 -- normal vector is a special type of vector
 type NormalVector = TVector3
