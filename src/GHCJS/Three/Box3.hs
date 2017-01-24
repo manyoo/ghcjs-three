f5fdaa02f5956f866622f7fe9bd1e89a0eb00c30
Eric Wong <ericsyw@gmail.com>
2017-01-24 21:10:54 +0800

    Add new APIs for Box3

--- src/GHCJS/Three/Box3.hs
+++ src/GHCJS/Three/Box3.hs
@@ -4,7 +4,26 @@ module GHCJS.Three.Box3 where
 import GHCJS.Types
 
 import GHCJS.Three.Monad
+import GHCJS.Three.Vector
 
 newtype Box3 = Box3 {
     box3Object :: BaseObject
     } deriving ThreeJSVal
+
+foreign import javascript unsafe "($1)['min']"
+    thr_boxMin :: JSVal -> Three JSVal
+
+foreign import javascript unsafe "($1)['max']"
+    thr_boxMax :: JSVal -> Three JSVal
+
+foreign import javascript unsafe "($1)['getCenter']()"
+    thr_boxCenter :: JSVal -> Three JSVal
+
+boxMin :: Box3 -> Three Vector3
+boxMin b = thr_boxMin (toJSVal b) >>= toVector3 . fromJSVal
+
+boxMax :: Box3 -> Three Vector3
+boxMax b = thr_boxMax (toJSVal b) >>= toVector3 . fromJSVal
+
+boxCenter :: Box3 -> Three Vector3
+boxCenter b = thr_boxCenter (toJSVal b) >>= toVector3 . fromJSVal
